99bfc89f51c854246f1bfc6a6ba737df47d586d9
Caitlin Lauer <caitlinrinn@gmail.com>
2018-07-05 22:05:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {
+  prop: "1",
+  prop3: "3"
+};
+
+function updateObjectWithKeyAndValue(obj,key,value) {
+   return Object.assign({}, recipes, { ['prop2']: "2" })
+   return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes["new key"] = "sixteen";
+  return recipes;
+}
+
+
+function deleteFromObjectByKey(object, key) {
+  var recipes2 = Object.assign({}, recipes2);
+  delete recipes2.prop;
+  return recipes2;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  delete recipes.prop;
+ return recipes;
+}
